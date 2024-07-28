9fdfc9a6b8617c048d8006970bd8c777c28bc00f
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-07-28 17:57:07 -0300

    Adicionando o media query

--- Exercicios/ex026/mq005/JS/script.js
+++ Exercicios/ex026/mq005/JS/script.js
@@ -1,5 +1,14 @@
-function clickMenu() {
+function mudouTamanho() {
 
+    if(window.innerWidth >= 768) {
+        menu.style.display = 'inline-block'
+    } else {
+        menu.style.display = 'none';
+    }
+}
+
+function clickMenu() {
+    
     if(menu.style.display == 'block') {
 
         menu.style.display = 'none';
@@ -7,3 +16,4 @@ function clickMenu() {
         menu.style.display = 'block';
     }
 }
+
